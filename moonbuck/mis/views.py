0dfc53d6df0ab0281ae518671da1274b3820ed8a
SLAPaper <slapaper.pku@gmail.com>
2015-01-20 02:16:26 +0800

    commit before merge

--- moonbuck/mis/views.py
+++ moonbuck/mis/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 from django.http import HttpResponse,Http404
-from django.template import Template,Context
+from django.template import Template, Context, RequestContext
 from django.template.loader import get_template
 from django.shortcuts import render_to_response
 import datetime
@@ -16,39 +16,26 @@ def homepage(request):
 def crmHome(request):
     return render_to_response('CRM首页.html',locals())
 
-def crmSearch(request):
-    return render_to_response('CRM检索页面.html',locals())
-
-def crmSearchResult(request):
-    if request.method == 'GET':
-        query = request.GET
-        pass
-    elif request.method =='POST':
-        query = request.POST
-        pass
-    else:
-        return render_to_response('CRM查询结果.html',locals())
-
 def prmHome(request):
     return render_to_response('PRM首页.html',locals())
 
 def crm_adduser(request):
-    errors=[]
+    errors = []
     if request.method == 'POST':
         if not errors:
-            userno=request.GET['userno']
-            username=request.GET['username']
-            email=request.GET['email']
-            mob_num=request.GET['mob_num']
-            birthyear=request.GET['birthyear']
-            birthmonth=request.GET['birthmonth']
-            birthday=request.GET['birthday']
-            address=request.GET['address']
-            cryear=request.GET['cryear']
-            crmonth=request.GET['crmonth']
-            crday=request.GET['crday']
+            userno = request.GET['userno']
+            username = request.GET['username']
+            email = request.GET['email']
+            mob_num = request.GET['mob_num']
+            birthyear = request.GET['birthyear']
+            birthmonth = request.GET['birthmonth']
+            birthday = request.GET['birthday']
+            address = request.GET['address']
+            cryear = request.GET['cryear']
+            crmonth = request.GET['crmonth']
+            crday = request.GET['crday']
 
-            temp=customer(cuId=userno,cuName=username,cuEmail=email,cuPhone=mob_num,cuCreate=crday)
+            temp = customer(cuId=userno,cuName=username,cuEmail=email,cuPhone=mob_num,cuCreate=crday)
             temp.save()
 
             return HttpResponseRedirect('/crm/')
@@ -62,10 +49,14 @@ def crm_user_search(request):
 def crm_user_searchresult(request):
     if request.method == 'GET':
         if 'participant1' & 'textcontent1' in request.GET:
-            participant1=request.GET['participant1']
-            textcontent1=request.GET['textcontent1']
+            participant1 = request.GET['participant1']
+            textcontent1 = request.GET['textcontent1']
             if participant1 == 2:
-                record=mis.objects.filter(cuName=textcontent1)
+                record = mis.objects.filter(cuName=textcontent1)
             return render_to_response('CRM查询结果.html',locals())
         else:
             return render_to_response('CRM检索页面.html',locals())
+    elif request.method == 'POST':
+        query = request.POST
+        if query.get('participant1') != 1:
+            return HttpResponse("nothing to response")
